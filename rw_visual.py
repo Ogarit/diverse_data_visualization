18f44bb966f71cd649b97f51618063d4ed08568b
Tiago Marques Lima <tiagomarquesdados@gmail.com>
2025-10-21 18:41:37 -0300

    Add loop to generate multiple random walks
    The script now repeatedly generates and displays random walks until the user chooses to exit. This allows for interactive exploration of different random walk patterns.

--- rw_visual.py
+++ rw_visual.py
@@ -1,14 +1,20 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-# Cria um random walk
-rw = RandomWalk()
-rw.fill_walk()
+# Continua criando passeios novos desde que o programa esteja ativo
+while True:
+    # Cria um random walk
+    rw = RandomWalk()
+    rw.fill_walk()
 
-# Plota os pontos no passeio
-plt.style.use('seaborn-v0_8')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-ax.set_aspect('equal')
+    # Plota os pontos no passeio
+    plt.style.use('seaborn-v0_8')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+    ax.set_aspect('equal')
 
-plt.show()
+    plt.show()
+
+    keep_running = input("Fazer outra caminhada? (s/n): ")
+    if keep_running.lower() == 'n':
+        break
