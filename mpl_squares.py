3b2dc5e62cbedae1bf00278133fb6b164191f3d5
Tiago Marques Lima <tiagomarquesdados@gmail.com>
2025-10-21 17:17:19 -0300

    Add titles and labels to matplotlib squares plot
    Enhanced the plot by setting a title, axis labels, and tick label sizes. Also increased the line width for better visibility.

--- mpl_squares.py
+++ mpl_squares.py
@@ -3,6 +3,14 @@ import matplotlib.pyplot as plt
 squares = [1, 4, 9, 16, 25]
 
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares, linewidth=3)
+
+# Define o título do gráfico e os eixos do rótulo
+ax.set_title("Números Quadrados", fontsize=14)
+ax.set_xlabel("Valor", fontsize=14)
+ax.set_ylabel("Quadrados dos Valores", fontsize=14)
+
+# Define o tamanho dos rótulos de marcação de escala
+ax.tick_params(labelsize=14)
 
 plt.show()
