5e56d103396a3bbe4d075808049f9efea91165eb
Tiago Marques Lima <tiagomarquesdados@gmail.com>
2025-10-23 17:08:27 -0300

    Create die_visual_plt.py
    A visualization version of die_visual.py using Matplotlib.

---
+++ die_visual_plt.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+from die import Die
+
+# Cria um D6
+die = Die()
+
+# Realiza alguns testes e armazena os resultados em uma lista
+results = [die.roll() for _ in range(1_000_000)]
+
+# Analisa os resultados
+poss_results = range(1, die.num_sides+1)
+frequencies = [results.count(value) for value in poss_results]
+
+# Visualiza os resultados
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.bar(poss_results, frequencies)
+
+ax.set_title("Resultados da Rolagem de um D6 1.000.000 Vezes")
+ax.set_xlabel("Resultado")
+ax.set_ylabel("Frequência do Resultado")
+
+plt.show()
