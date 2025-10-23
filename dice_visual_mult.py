d772119b4e3d023856e274e7253e87f35104a33f
Tiago Marques Lima <tiagomarquesdados@gmail.com>
2025-10-23 16:48:19 -0300

    Create dice_visual_mult.py
    Graph that calculates the probabilities of the results of multiplying the values ​​of rolling 2 6-sided dice.

---
+++ dice_visual_mult.py
@@ -0,0 +1,30 @@
+import plotly.express as px
+from die import Die
+
+# Cria dois D6
+die1 = Die()
+die2 = Die()
+
+# Realiza alguns testes e armazena os resultados em uma lista
+results = []
+for roll_num in range(1_000_000):
+    result = die1.roll() * die2.roll()
+    results.append(result)
+
+# Analisa os resultados
+frequencies = []
+max_results = die1.num_sides * die2.num_sides
+poss_results = range(1, max_results+1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualiza os resultados
+title = "Resultados da Multiplicação da Rolagem de dois D6 1.000.000 de Vezes"
+labels = {'x': 'Resultado', 'y': 'Frequência do Resultado'}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+
+# Personaliza ainda mais o gráfico
+fig.update_layout(xaxis_dtick=1)
+
+fig.show()
