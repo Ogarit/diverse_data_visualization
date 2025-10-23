7b7050cebbd602f87eb4f07851d986ecdddbb984
Tiago Marques Lima <tiagomarquesdados@gmail.com>
2025-10-22 22:35:20 -0300

    Update die_visual.py

--- die_visual.py
+++ die_visual.py
@@ -17,6 +17,8 @@ for value in poss_results:
     frequency = results.count(value)
     frequencies.append(frequency)
 
-# Visualiza os resultados
-fig = px.bar(x=poss_results, y=frequencies)
+# Visualiza os resultados~
+title = "Resultados da Rolagem de um D6 1.000 vezes"
+labels = {'x': 'Resultado', 'y': 'Frequência do Resultado'}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
 fig.show()
