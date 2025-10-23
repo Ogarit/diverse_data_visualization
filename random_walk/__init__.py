ec32d43f83082525c7d88d5166c12b28f05f77ac
Tiago Marques Lima <tiagomarquesdados@gmail.com>
2025-10-23 17:30:08 -0300

    Update RandomWalk

--- random_walk/__init__.py
+++ random_walk/__init__.py
@@ -12,8 +12,10 @@ class RandomWalk:
         self.x_values = [0]
         self.y_values = [0]
 
-    def fill_walk(self):
+    def fill_walk(self, has_z=False):
         """Calcula todos os pontos do passeio."""
+        if has_z:
+            self.z_values = [0]
 
         # Continua dando passos até que o passeio atinja o comprimento desejado
         while len(self.x_values) < self.num_points:
@@ -21,17 +23,27 @@ class RandomWalk:
             # Decide qual a direção tomar, e até onde ir
             x_step = self.get_step()
             y_step = self.get_step()
+            if has_z:
+                z_step = self.get_step()
 
             # Rejeita movimentos que não vão a lugar algum
             if x_step == 0 and y_step == 0:
-                continue
+                if has_z:
+                    if z_step == 0:
+                        continue
+                else:
+                    continue
 
             # Calcula a nova posição
             x = self.x_values[-1] + x_step
             y = self.y_values[-1] + y_step
+            if has_z:
+                z = self.z_values[-1] + z_step
 
             self.x_values.append(x)
             self.y_values.append(y)
+            if has_z:
+                self.z_values.append(z)
 
     def get_step(self):
         """Calcula as etapas da caminhada."""
