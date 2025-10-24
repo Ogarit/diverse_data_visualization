8e84bb32d32aa64b00007cd85a7f1193204411a0
Tiago Marques Lima <tiagomarquesdados@gmail.com>
2025-10-24 15:30:43 -0300

    Plotting data on a temperature graph

--- weather_data/sitka_highs.py
+++ weather_data/sitka_highs.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import csv
+import matplotlib.pyplot as plt
 
 path = Path('weather_data/sitka_weather_07-2021_simple.csv')
 lines = path.read_text().splitlines()
@@ -13,4 +14,15 @@ for row in reader:
     high = int(row[4])
     highs.append(high)
 
-print(highs)
+# Plota as temperaturas máximas
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(highs, color='red')
+
+# Formata o gráfico
+ax.set_title('Temperaturas Máxima Diária, Julho de 2021', fontsize=24)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel('Temperatura (Fº)', fontsize=16)
+ax.tick_params(labelsize=16)
+
+plt.show()
