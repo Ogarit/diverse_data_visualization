03a816e0c87cec2845eb54f63f7fe859ad3107a3
Tiago Marques Lima <tiagomarquesdados@gmail.com>
2025-10-24 15:47:34 -0300

    A shaded area of ​​the chart

--- weather_data/sitka_highs_lows.py
+++ weather_data/sitka_highs_lows.py
@@ -22,8 +22,9 @@ for row in reader:
 # Plota as temperaturas máximas e mínimas
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.plot(dates, highs, color='red')
-ax.plot(dates, lows, color='blue')
+ax.plot(dates, highs, color='red', alpha=0.5)
+ax.plot(dates, lows, color='blue', alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Formata o gráfico
 ax.set_title('Temperaturas Máxima e Mínima Diária, 2021', fontsize=24)
