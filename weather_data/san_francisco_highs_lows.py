596d3b3123a656d2fa8145bbd455ea8ce64a5668
Tiago Marques Lima <tiagomarquesdados@gmail.com>
2025-10-26 14:34:33 -0300

    San Francisco, CA Temperature Graph Plot

---
+++ weather_data/san_francisco_highs_lows.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from pathlib import Path
+import csv
+import matplotlib.pyplot as plt
+
+# Preparando o arquivo csv
+path = Path('weather_data/data/san_francisco_2021_full.csv')
+lines = path.read_text().splitlines()
+
+san_francisco = csv.reader(lines)
+next(san_francisco)
+
+# Obtendo as listas com os dados de temperatura máxima e mínima
+dates, tmax_list, tmin_list = [], [], []
+for row in san_francisco:
+    date = datetime.strptime(row[2], r'%Y-%m-%d')
+    tmax = int(row[7])
+    tmin = int(row[8])
+    dates.append(date)
+    tmax_list.append(tmax)
+    tmin_list.append(tmin)
+
+# Gerando a figura do gráfico
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+
+# Plotando o gráfico de linhas com a temperatura máxima e mínima
+ax.plot(dates, tmax_list, color='red', alpha=0.5)
+ax.plot(dates, tmin_list, color='blue', alpha=0.5)
+ax.fill_between(dates, tmax_list, tmin_list, facecolor='blue', alpha=0.1)
+
+# Estilizando o gráfico
+title = 'Temperaturas máximas e mínimas, San Francisco, CA, 2021'
+ax.set_title(title, fontsize=20)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel('Temperatura (Fº)')
+ax.set_ylim(10, 140)
+ax.tick_params(labelsize=16)
+
+fig.autofmt_xdate()
+
+plt.show()
