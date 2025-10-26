1e5e7637c7e8d9f729f340feb30b5c3027c2e8e9
Tiago Marques Lima <tiagomarquesdados@gmail.com>
2025-10-26 16:22:08 -0300

    Update eq_explore_data.py

--- eq_data/eq_explore_data.py
+++ eq_data/eq_explore_data.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import json
+import plotly.express as px
 
 # Lê os dados como uma string e os converte em um objeto Python
 path = Path('eq_data/data/eq_data_1_day_m1.geojson')
@@ -8,4 +9,16 @@ all_eq_data = json.loads(contents)
 
 # Examina todos os terremotos no conjunto de dados
 all_eq_dicts = all_eq_data['features']
-print(len(all_eq_dicts))
+
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+title = 'Global Earthquakes'
+fig = px.scatter_geo(lat=lats, lon=lons, title=title)
+fig.show()
